dcf6b74e010f197d5ff77e1107fb03bd4f4d5aae
smita Raj <smita.raj12@gmail.com>
2021-09-06 15:23:12 -0700

    application for Todo List

---
+++ TodoList/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+
+namespace ToDoList
+{
+  public class Program
+  {
+    public static List<string> items = new List<string> {};
+
+    public static void Main()
+    {
+      Console.WriteLine ("Welcome to the To Do List");
+      Console.WriteLine("----------------------------");
+      Console.WriteLine ("Would you like to add an item to your list or view your list?(Add/View)");
+      string UsrerOutput = Console.ReadLine();
+      if(UsrerOutput == "Add")
+      {
+        Console.WriteLine("Please enter the description for the new item");
+        string userItem = Console.ReadLine();
+        items.Add(userItem);
+        Main();
+      }
+      else if(UsrerOutput == "View")
+      {
+        foreach(string i in items)
+        {
+          Console.WriteLine(i);
+        }
+      }
+    }
+  }
+}      
