df4d5fa4d22337324fce6d5f1947909e0aca6741
smita Raj <smita.raj12@gmail.com>
2021-09-13 19:59:37 -0700

    todo list with web

--- TodoList/Models/ItemVariable.cs
+++ TodoList/Models/ItemVariable.cs
@@ -4,20 +4,24 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description { get; set;}
+    public string Description { get; set; }
     private static List<Item> _instances = new List<Item> {};
-    public Item(string description)
+
+    public Item (string description)
     {
       Description = description;
       _instances.Add(this);
     }
+
     public static List<Item> GetAll()
     {
       return _instances;
     }
+
     public static void ClearAll()
     {
       _instances.Clear();
     }
+
   }
 }
--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,4 +1,30 @@
-using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ToDoList
+{
+   public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
+
+
+
+
+
+
+/*using System;
 using System.Collections.Generic;
 using ToDoList.Models;
 
@@ -32,3 +58,4 @@ namespace ToDoList
     }
   }
 }      
+*/
